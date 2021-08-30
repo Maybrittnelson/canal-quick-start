50e4fe587b69bcc492746a877362c50167e0b1b6
geshaofeng <geshaofeng@souche.com>
2021-08-30 22:08:24 +0800

    feature/init: add sql & test app

---
+++ src/main/resources/sql/test_db.sql
@@ -0,0 +1,6 @@
+--use test
+CREATE TABLE `xdual` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `x` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
